58a5c6e556950ce091bbdbf96dbc980d4cb82112
Abdullah Fahlo <abdullahfahlo.com@gmail.com>
2025-11-10 04:01:22 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -351,7 +351,7 @@ st.dataframe(performance_df.style.format({
 st.info("✅ **Random Forest** selected as production model for optimal balance of accuracy and interpretability")
 
 st.markdown("""
-🧠 **NLP Model for Customer Feedback Analysis**
+🧠 #### NLP Model for Customer Feedback Analysis
 
 Customer reviews are processed using a transformer-based sentiment analysis model (DistilBERT).  
 This model converts free-text feedback into a numerical sentiment score ranging from **-1 (negative)** to **+1 (positive)**,  
